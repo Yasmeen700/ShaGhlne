78ec44c80d4ef0b34757a2b15b0f9980e675674d
AnasL <anoslaham@gmail.com>
2020-10-30 01:55:10 -0700

    implementing the bottom navigation bar and starting the work on the form

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_app1/constant/strings.dart';
-
+import './ui/add_job_listing.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //   visualDensity: VisualDensity.adaptivePlatformDensity,
       // ),
-       home: MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -30,11 +30,36 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(Strings.AppName,style: TextStyle(color: Colors.black),),
+        title: Text(
+          Strings.AppName,
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.blueAccent,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
       ),
       body: Container(
         color: Color(0xff1c1922),
-
+        child: Column(
+          children: [
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return AddJob();
+                }));
+              },
+              child: Text('Hello'),
+            )
+          ],
+        ),
       ),
     );
   }
