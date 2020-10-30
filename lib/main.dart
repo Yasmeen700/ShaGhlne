b9cecbef4b864702fec82900fa9a26cbd529b63f
Yasmeen700 <b7Xy8e5XUZPABAAIrO31nUQPcCnWDIoB>
2020-10-30 17:10:10 +0200

    Merge branch 'master' of https://github.com/Yasmeen700/shaghlne into master
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app1/constant/strings.dart';
 import './ui/add_job_listing.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:get/get.dart';
 void main() {
   runApp(MyApp());
 }
@@ -9,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
 
       title: Strings.AppName,
       theme: ThemeData.dark(),
@@ -35,14 +36,15 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       bottomNavigationBar: CurvedNavigationBar(
+        index: 1,
         height: 50,
         animationDuration: Duration(milliseconds: 150),
         animationCurve: Curves.bounceInOut,
         backgroundColor: Color(0xff1c1922),
         items: <Widget>[
           Icon(Icons.add, size: 30,color:Color(0xff1c1922),),
-          Icon(Icons.list, size: 30,color: Color(0xff1c1922),),
-          Icon(Icons.compare_arrows, size: 30,color: Color(0xff1c1922),),
+          Icon(Icons.menu_rounded, size: 30,color: Color(0xff1c1922),),
+          Icon(Icons.save_alt, size: 30,color: Color(0xff1c1922),),
         ],
         onTap: (index) {
         },
@@ -57,7 +59,8 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) {
                   return AddJob();
-                }));
+                }
+                ));
               },
               child: Text('Hello'),
             )
