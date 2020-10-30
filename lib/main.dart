da882c6b345bd3a6909273ebfe22c7b94824f41e
Yasmeen700 <b7Xy8e5XUZPABAAIrO31nUQPcCnWDIoB>
2020-10-30 13:46:36 +0200

    message

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-  import 'package:flutter/material.dart';
+  import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_app1/constant/strings.dart';
 
 void main() {
@@ -29,13 +30,41 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(Strings.AppName,style: TextStyle(color: Colors.black),),
+
+        backgroundColor: Color(0xff1c1922),
+        centerTitle: true,
+        title: Text(Strings.AppName,style: TextStyle(color: Colors.white),),
       ),
       body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         color: Color(0xff1c1922),
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('demo text',
+            style: TextStyle( color:Colors.white),),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 1,
+        animationCurve: Curves.bounceInOut,
+        animationDuration: Duration(milliseconds: 100),
+        color:Colors.white ,
+          backgroundColor:Color(0xff1c1922) ,
+          buttonBackgroundColor:  Colors.white,
+          height: 50.0,
+          items: <Widget>[
+            Icon(Icons.add, size: 30,color: Color(0xff1c1922),),
+            Icon(Icons.list, size: 30,color:Color(0xff1c1922),),
+            Icon(Icons.compare_arrows, size: 30,color:Color(0xff1c1922),),
+          ],
+              onTap: (index){
+            debugPrint('current index is $index');
+    },
       ),
     );
+
   }
 }
